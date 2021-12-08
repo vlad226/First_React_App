f53c9a57c5c2962ca05d8a10c174406ba8d9580a
Vladislav <ebermanvladislav@gmail.com>
2021-12-08 17:36:58 +0300

    Add new progect

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import React, { useState} from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+  return (
+    <li>
+    <Card className='expense-item'>
+     <ExpenseDate date = {props.date}/> 
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+      </div>
+      <div className='expense-item__price'> ${props.amount}</div>
+    </Card>
+    </li>
+    
+  );
+}
+export default ExpenseItem;
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
+import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+    
+  });
+  
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+
+        <ExpensesList items={filteredExpenses}/>
+      </Card>
+    </div>
+  );
+};
+export default Expenses;
